07d05d9e19b4efe6058c0e2548ab66c007f52d4a
Santiago Eid <santiagoeid93@gmail.com>
2025-09-25 11:21:09 -0400

    feat(draft): Add first pass of the CLI flow.

--- src/program.ts
+++ src/program.ts
@@ -1,9 +1,27 @@
-import { FizzBuzzCallenge } from "./challenges/fizzBuzz.ts";
-import { getChallengeData } from "./static/index.ts";
 
-(function() {
-  console.log('Hello!');
-  const config = getChallengeData('fizzBuzz');
-  const Challenge = new FizzBuzzCallenge(config);
-  console.log(Challenge.name, Challenge.difficulty);
+import { select } from "@inquirer/prompts";
+
+import { getChallengeData } from "./static/index.ts";
+import {
+  getChallenge,
+  getAvailableChallenges
+} from "./challenges/index.ts";
+import type { FileNameMap } from "./types.js";
+
+
+(async function program(): Promise<void> {
+  const challenges: FileNameMap[] = getAvailableChallenges();
+  
+  const selectedChallenge: string = await select({
+    message: 'Select a challenge to start: ',
+    choices: challenges
+  });
+
+  if (!selectedChallenge) {
+    console.log('No challenge selected. Exiting...');
+    process.exit(0);
+  }
+
+  const challenge = await getChallenge(selectedChallenge);
+  console.log(challenge.name);
 }());
