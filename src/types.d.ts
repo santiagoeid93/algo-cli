8657ff68fed272dbfd868a8f9e2066dd643083c3
Santiago Eid <santiagoeid93@gmail.com>
2025-10-09 11:14:28 -0400

    docs: Port some types from node util.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,5 @@
+import util from 'util';
+
 export type Difficulty = 'easy' | 'medium' | 'hard';
 
 export type Action = 'start' | 'test' | 'view' | 'delete' | 'exit';
@@ -24,3 +26,63 @@ export type CliPrompt = {
   message: string;
   choices: CliStep[];
 }
+
+export type TextStyles = ForegroundColors | BackgroundColors | Modifiers | (ForegroundColors | BackgroundColors | Modifiers)[];
+
+// Ported this from 'node:util:types' since they are non exported members.
+
+// https://nodejs.org/docs/latest/api/util.html#foreground-colors
+    type ForegroundColors =
+        | "black"
+        | "blackBright"
+        | "blue"
+        | "blueBright"
+        | "cyan"
+        | "cyanBright"
+        | "gray"
+        | "green"
+        | "greenBright"
+        | "grey"
+        | "magenta"
+        | "magentaBright"
+        | "red"
+        | "redBright"
+        | "white"
+        | "whiteBright"
+        | "yellow"
+        | "yellowBright";
+    // https://nodejs.org/docs/latest/api/util.html#background-colors
+    type BackgroundColors =
+        | "bgBlack"
+        | "bgBlackBright"
+        | "bgBlue"
+        | "bgBlueBright"
+        | "bgCyan"
+        | "bgCyanBright"
+        | "bgGray"
+        | "bgGreen"
+        | "bgGreenBright"
+        | "bgGrey"
+        | "bgMagenta"
+        | "bgMagentaBright"
+        | "bgRed"
+        | "bgRedBright"
+        | "bgWhite"
+        | "bgWhiteBright"
+        | "bgYellow"
+        | "bgYellowBright";
+    // https://nodejs.org/docs/latest/api/util.html#modifiers
+    type Modifiers =
+        | "blink"
+        | "bold"
+        | "dim"
+        | "doubleunderline"
+        | "framed"
+        | "hidden"
+        | "inverse"
+        | "italic"
+        | "none"
+        | "overlined"
+        | "reset"
+        | "strikethrough"
+        | "underline";
